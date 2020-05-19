1bbcb95d9c8f30c1f32cf633832abbf6533a1430
anastasiagladkova <kipelova.stasy@mail.ru>
2020-05-19 22:12:28 +0300

    Initial Commit

---
+++ Notes flipping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes flipping
+//
+//  Created by Анастасия Гладкова on 19.05.2020.
+//  Copyright © 2020 Анастасия Гладкова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
